c33f2c6f9cb698a72ba552b7dd4562d02e7ce01a
prk4224 <prk0487@gmail.com>
2022-03-15 14:02:33 +0900

    Baekjoon0315

---
+++ src/Algorithm/Back_j_10799_1.java
@@ -0,0 +1,41 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Back_j_10799_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+
+        String str = br.readLine();
+
+        br.close();
+        char[] ch = new char[str.length()];
+
+        for(int i = 0; i < str.length(); i++){
+            ch[i] = str.charAt(i);
+        }
+
+        int cut = 0;
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] =='(' && ch[i+1] ==')'){
+                cut += stack.size();
+                i += 1;
+            }
+            else if(ch[i] == '('){
+                stack.push(ch[i]);
+            }
+            else {
+                cut++;
+                stack.pop();
+            }
+        }
+
+
+        System.out.print(cut);
+
+    }
+}
