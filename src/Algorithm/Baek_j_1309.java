c72fc272acd30f9f08224d904e7bc541e8814177
Jae_Hong <prk0487@gmail.com>
2022-03-23 18:42:37 +0900

    20220323

---
+++ src/Algorithm/Baek_j_1309.java
@@ -0,0 +1,25 @@
+package Algorithm;
+
+import java.io.*;
+
+public class Baek_j_1309 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+         // 사자의 최대 마리수
+
+        long dp[] = new long[N+1];
+        dp[0] = 1;
+        dp[1] = 3;
+        for(int i = 2; i <= N; i++){
+            dp[i] = (dp[i-1]*2 + dp[i-2])%9901;
+        }
+
+        bw.write(dp[N] +"");
+        bw.flush();
+        bw.close();
+    }
+}
