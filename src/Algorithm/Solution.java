2f4055e67a9084652a603566c54df13be71a613f
Jae_Hong <prk0487@gmail.com>
2022-03-28 18:31:16 +0900

    20220328

---
+++ src/Algorithm/Solution.java
@@ -0,0 +1,34 @@
+
+package Algorithm;
+
+//dfs 그룹 개수 구하기
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int computers[][] = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        boolean visited[] = new boolean[computers.length];
+
+        int answer = 0;
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i,visited,computers);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static boolean dfs(int x, boolean visited[] ,int computers[][]) {
+        visited[x] = true;
+
+        for (int i = 0; i < computers[x].length; i++) {
+            if (computers[x][i] == 1 && !visited[i]) {
+                dfs(i,visited,computers);
+            }
+        }
+        return true;
+
+    }
+}
+
