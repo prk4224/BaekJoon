3827afcc09452bbe4e17dfdfd68f0f3f6d23a296
Jae_Hong <prk0487@gmail.com>
2022-04-30 18:38:50 +0900

    20220430

---
+++ src/Algorithm/Beak_j_1931.java
@@ -0,0 +1,50 @@
+//'활동 선택 문제(Activity Selection problem)'
+
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Beak_j_1931 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int time[][] = new int[N][2];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                // 끝나는 시간이 같으면 시작 시작 순으로 정렬
+                if(o1[1] == o2[1]){
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cut = 0;
+        int prev = 0;
+
+        for(int i = 0; i < N; i++){
+            if(prev <= time[i][0]){
+                prev = time[i][1];
+                cut++;
+            }
+        }
+
+        System.out.println(cut);
+    }
+}
