713e4ec37cbf5e020c142d0bc540fbedf5414797
박재홍 <prk0487@gmail.com>
2022-06-12 16:16:28 +0900

    20220612

--- src/Algorithm/Baek_j_1080.java
+++ src/Algorithm/Baek_j_1080.java
@@ -1,12 +1,12 @@
 package Algorithm;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*
+그리디 문제 : 순서대로 진행해서 문자가 일치하지 않으면 바꾼다. 그럼 그 자리 값이 같게 고정이 됨
+ */
 public class Baek_j_1080 {
     public static String[] strArr;
     public static void main(String[] args) throws IOException{
@@ -30,6 +30,7 @@ public class Baek_j_1080 {
             targetArr[i] = br.readLine();
         }
 
+        // N,M이 3보다 작으면 트레이드가 이루워지지 않기 때문에 행렬을 비교해서 같으면 0, 다르면 1을 출력하도촉 한다.
         if(N < 3 || M < 3){
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < M; j++){
@@ -41,9 +42,9 @@ public class Baek_j_1080 {
             }
             System.out.print(0);
             return;
-
         }
 
+        // 해당 자리 값을 비교했을때 바르면 트레이드를 진행한다.
         for(int i = 0; i < N-2; i++){
             for(int j = 0; j < M-2; j++){
                 if(strArr[i].charAt(j) != targetArr[i].charAt(j)){
@@ -53,16 +54,19 @@ public class Baek_j_1080 {
             }
         }
 
-
-
-        if(strArr[N-2].charAt(M-1) == targetArr[N-2].charAt(M-1)){
-            System.out.print(result);
+        // 비교가는 행령의 위치가 3 보다 작으면 트레이드가 이루워 지지 않기 때문에 3 이하의 값 아
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (strArr[N-i-1].charAt(M-j-1) != targetArr[N-i-1].charAt(M-j-1)) {
+                    System.out.print(-1);
+                    return;
+                }
+            }
         }
-        else{
-            System.out.print(-1);
-        }
+        System.out.print(result);
     }
 
+    // 3x3 행령 만큼 바꾸는 함수
     public static void change(int N, int M){
 
         for(int i = 0 ; i < 3; i++){
---
+++ src/Algorithm/Baek_j_1620.java
@@ -0,0 +1,41 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Baek_j_1620 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String,Integer> p_list_1 = new HashMap<>();
+        Map<Integer,String> p_list_2 = new HashMap<>();
+
+        for(int i = 1; i <= N; i++){
+            String str = br.readLine();
+            p_list_1.put(str,i);
+            p_list_2.put(i,str);
+        }
+
+        for(int i = 0; i < M; i++){
+            String str = br.readLine();
+
+            if(str.charAt(0) > '0' && str.charAt(0) <= '9'){
+                sb.append(p_list_2.get(Integer.parseInt(str))).append("\n");
+            }
+            else {
+                sb.append(p_list_1.get(str)).append("\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
