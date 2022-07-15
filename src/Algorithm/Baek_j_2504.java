7421561bd62dc321a487d9dd2219008ad2b3d580
박재홍 <prk0487@gmail.com>
2022-07-15 16:27:50 +0900

    20220715

--- src/Algorithm/Baek_j_2504.java
+++ src/Algorithm/Baek_j_2504.java
@@ -3,16 +3,35 @@ package Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Baek_j_2504 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
 
         String input = br.readLine();
+        int result = 0;
 
         for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == '(' || input.charAt(i) == '['){
+                stack.push(input.charAt(i));
+            }
+            else if(input.charAt(i) == ')'){
 
+                if(stack.empty()) {
+                    System.out.println(0);
+                    return;
+                }
+
+                while (true){
+                    char ch = stack.pop();
+
+                    if(ch == '(')
+                }
+
+            }
         }
 
     }
