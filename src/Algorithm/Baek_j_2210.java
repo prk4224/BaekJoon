86dce4b8da6033b038cc4c08fd59bf391a31ed9f
박재홍 <prk0487@gmail.com>
2022-07-20 19:18:31 +0900

    20220720

---
+++ src/Algorithm/Baek_j_2210.java
@@ -0,0 +1,12 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_j_2210 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
